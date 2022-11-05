3d0412bfa16a89a594021a3785e6e4e65f92fcbd
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-05 22:23:52 +0800

    generated route access

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'cubits/counter/counter_cubit.dart';
+import 'show_me_counter_page.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final CounterCubit _counterCubit;
+
+  @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterCubit>(
-      create: (ctx) => CounterCubit(),
-      child: MaterialApp(
-        title: 'Bloc Access',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomePage(),
+    return MaterialApp(
+      title: 'Bloc Access: Generated Route Access',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case HomePage.id:
+            return MaterialPageRoute(
+              settings: settings,
+              builder: (ctx) => BlocProvider.value(
+                value: _counterCubit,
+                child: const HomePage(),
+              ),
+            );
+          case ShowMeCounterPage.id:
+            return MaterialPageRoute(
+              settings: settings,
+              builder: (ctx) => BlocProvider.value(
+                value: _counterCubit,
+                child: const ShowMeCounterPage(),
+              ),
+            );
+        }
+
+        return null;
+      },
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _counterCubit = CounterCubit();
+  }
+
+  @override
+  void dispose() {
+    _counterCubit.close();
+    super.dispose();
   }
 }
 
 class HomePage extends StatelessWidget {
+  static const id = '/';
+
   const HomePage({super.key});
 
   @override
@@ -32,13 +72,12 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            BlocSelector<CounterCubit, CounterState, int>(
-              selector: (state) => state.counter,
-              builder: (ctx, counter) => Text(
-                '$counter',
-                style: const TextStyle(
-                  fontSize: 52,
-                ),
+            ElevatedButton(
+              onPressed: () =>
+                  Navigator.pushNamed(context, ShowMeCounterPage.id),
+              child: const Text(
+                'Show Me Counter',
+                style: TextStyle(fontSize: 18),
               ),
             ),
             const SizedBox(height: 15),
