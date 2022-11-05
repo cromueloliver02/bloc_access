7be3f9e9a77a494087655addfdcc10950015fb39
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-05 14:20:13 +0800

    project setup

---
+++ lib/cubits/counter/counter_cubit.dart
@@ -0,0 +1,9 @@
+// ignore: depend_on_referenced_packages
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState.initial());
+}
